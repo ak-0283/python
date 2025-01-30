3ca0680814683193473521944be37d1b785ce137
abhaykumar-0285 <abhay.kr2803@gmail.com>
2025-01-30 21:07:32 +0530

    full lecture was covered

--- b-a/01/app.py
+++ b-a/01/app.py
@@ -2,3 +2,10 @@ print("hello world")
 
 # shift + ctrl + p to open command palette
 # ctrl + shift + m to to open problems tab
+
+
+
+# implementations
+# jython -> java
+# ironpython -> c#
+# pypy -> subset of python
