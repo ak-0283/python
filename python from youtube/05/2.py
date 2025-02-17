6cb956e2797156f4b6a76d52686441df9f61219e
abhaykumar-0285 <abhay.kr2803@gmail.com>
2025-02-17 20:09:08 +0530

    half part done

---
+++ python from youtube/05/2.py
@@ -0,0 +1,25 @@
+# break statement
+# i=1
+# while i<=5:
+#     print(i)
+#     if(i==3):
+#         break
+#     i+=1
+
+# print("end of loop")
+
+
+# continue statement
+# i=1
+# while i<=5:
+#     if(i==3):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+
+# for loop
+nums=[1,2,3,4,5]
+for val in nums:
+    print(val)  
