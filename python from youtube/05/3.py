ce2ccf86912ff9371155bf9dc9899929d70e707c
abhaykumar-0285 <abhay.kr2803@gmail.com>
2025-02-24 21:04:59 +0530

    done whole lecture

---
+++ python from youtube/05/3.py
@@ -0,0 +1,10 @@
+# range
+# seq = range(5)
+
+# for i in seq:
+#     print(i)
+
+
+# print the set of even numbers
+for i in range(0,10,2):
+    print(i)
---
+++ python from youtube/05/practise/8.py
@@ -0,0 +1,16 @@
+# range
+# print the numbers of 1 to 100
+# for i in range(1,101,1):
+#     print(i)
+
+
+# print the numbers of 1 to 100
+# for i in range(100,0,-1):
+#         print(i)
+
+
+
+# print the multiplication table of a number n
+n = int(input("enter number: "))
+for i in range(1,11):
+    print(n*i)
