196a23a986e757e618b3505483860a1a97f7d39c
abhaykumar-0285 <abhay.kr2803@gmail.com>
2025-02-26 18:50:24 +0530

    11mins lecture done

---
+++ python from youtube/06/1.py
@@ -0,0 +1,16 @@
+# function
+
+# def calc_sum(a,b):
+#     sum = a+b;
+#     print(sum)
+#     return sum
+
+# calc_sum(5,10)
+
+
+# or easiest way to do 
+def calc_sum(a,b):
+    return a+b
+
+sum = calc_sum(1,3)
+print(sum)
