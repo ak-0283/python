0f72bf5cf64e488ed1538505d3ef56659ca3ca81
abhaykumar-0285 <abhay.kr2803@gmail.com>
2025-03-06 18:35:18 +0530

    20 mins lecture done

--- python from youtube/07/1.py
+++ python from youtube/07/1.py
@@ -4,3 +4,17 @@
 # print(data)
 # print(type(data))
 # f.close()
+
+
+# f = open("demo.txt","r")
+
+# line1 = f.readline()
+# print(line1)
+
+# f.close()
+
+
+
+f=open("demo.txt","w")
+line = f.write("hello world")
+f.close()
